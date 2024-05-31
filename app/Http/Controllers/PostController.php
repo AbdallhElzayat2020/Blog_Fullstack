0d6d5a520f28332ee5e3e34f9002d1b5ca0ceb35
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2024-05-31 05:22:28 +0300

    make Cache

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,64 +1,68 @@
 <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-use App\Models\post;
-use Illuminate\Http\Request;
+    use App\Models\post;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Cache;
 
-class PostController extends Controller
-{
-    /** * Display a listing of the resource. */
-    public function index(Request $request)
+    class PostController extends Controller
     {
-        $fileName = time().'_'.$request->image->getClientOriginalName();
-        $file_Path = $request->image->storeAs('uploads', $fileName);
-    }
+        /** * Display a listing of the resource. */
+        public function index( Request $request )
+        {
+            $fileName = time() . '_' . $request->image->getClientOriginalName();
+            $file_Path = $request->image->storeAs('uploads' , $fileName);
+//            $posts = Cache::rememberForever("posts" , function () {
+//                return post::all();
+//            });
+        }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+        /**
+         * Show the form for creating a new resource.
+         */
+        public function create()
+        {
+            //
+        }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+        /**
+         * Store a newly created resource in storage.
+         */
+        public function store( Request $request )
+        {
+            //
+        }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(post $post)
-    {
-        //
-    }
+        /**
+         * Display the specified resource.
+         */
+        public function show( post $post )
+        {
+            //
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(post $post)
-    {
-        //
-    }
+        /**
+         * Show the form for editing the specified resource.
+         */
+        public function edit( post $post )
+        {
+            //
+        }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, post $post)
-    {
-        //
-    }
+        /**
+         * Update the specified resource in storage.
+         */
+        public function update( Request $request , post $post )
+        {
+            //
+        }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(post $post)
-    {
-        //
+        /**
+         * Remove the specified resource from storage.
+         */
+        public function destroy( post $post )
+        {
+            //
+        }
     }
-}
--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,28 @@
 <?php
 
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\PostController;
+    use Illuminate\Support\Facades\Mail;
+    use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider and all of them will
+    | be assigned to the "web" middleware group. Make something great!
+    |
+    */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/sendemail', function () {
-    Mail::raw('hello world from Laravel here', function ($message) {
-        $message->to('admin@gmail.com')->subject('noreplay');
+    Route::get('/' , function () {
+        return view('welcome');
     });
-    dd('success');
-});
+
+    Route::get('/sendemail' , function () {
+        Mail::raw('hello world from Laravel here' , function ( $message ) {
+            $message->to('admin@gmail.com')->subject('noreplay');
+        });
+        dd('success');
+    });
+    Route::resource("data" , PostController::class);
